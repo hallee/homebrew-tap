15db4e725969a3fd9cc193439f73d57873a04b22
Hal <hal@lee.me>
2024-11-03 12:16:30 -0500

    Dotfiles 1.0.5

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,8 +1,8 @@
 class Dotfiles < Formula
   homepage "https://hal.codes"
-  version "1.0.4"
-  url "https://github.com/hallee/dotfiles/archive/1.0.4.tar.gz"
-  sha256 "2c69dc38e4c6d8cda33f977fc076f0d77a42576566116d0bb0600aeb3540e137"
+  version "1.0.5"
+  url "https://github.com/hallee/dotfiles/archive/1.0.5.tar.gz"
+  sha256 "f18e5d23c33115c28fdf1c5754573bd77b6201dbebb4b10a6caa3b5ad39dc73d"
 
   def install
     bin.install "Release/dotfiles"
