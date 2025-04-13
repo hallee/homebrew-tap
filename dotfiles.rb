d08d99dc04947103ff1ce42f29869824d84c821b
Hal <hal@lee.me>
2025-04-13 09:50:27 -0400

    Update dotfiles.rb

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,8 +1,8 @@
 class Dotfiles < Formula
   homepage "https://hal.codes"
-  version "1.0.8"
-  url "https://github.com/hallee/dotfiles/archive/1.0.8.tar.gz"
-  sha256 "ca4c71a2933024714af0e2ebab1d0c45637688a710ed3d87e8cee3d4b8a71c3c"
+  version "1.0.9"
+  url "https://github.com/hallee/dotfiles/archive/1.0.9.tar.gz"
+  sha256 "0a264a6652392c4a9e142aa3005fdbf8e2b76e514bf52292bde46b0e445d1f0e"
 
   def install
     bin.install "Release/dotfiles"
